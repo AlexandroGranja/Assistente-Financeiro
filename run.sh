70a28669a29ae4ca4ce3fb9ce6f40ede9e5a7170
Alexandro Granja <Alex.granjaaa@Hotmail.com>
2025-07-18 15:53:17 -0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,6 +2,11 @@
 
 echo "--- A iniciar o processo de deploy com Application Factory ---"
 
-# Inicia o Gunicorn chamando a função create_app()
-# O formato é 'modulo:nome_da_funcao()'
-gunicorn --bind 0.0.0.0:$PORT 'src.main:create_app()'
+# Inicia o Gunicorn com a flag --unbuffered para mostrar os logs imediatamente
+# Usamos 'python -m src.init_db' para executar o ficheiro como um módulo
+echo "A executar o script de inicialização do banco de dados..."
+python -m src.init_db
+
+# Inicia o Gunicorn com a flag --unbuffered
+echo "A iniciar o servidor Gunicorn com logs imediatos..."
+gunicorn --unbuffered --bind 0.0.0.0:$PORT 'src.main:create_app()'

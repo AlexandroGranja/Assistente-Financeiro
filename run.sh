a1bed30c5334670e7c8c7fd5e7faa900a7a098bd
Alexandro Granja <Alex.granjaaa@Hotmail.com>
2025-07-18 12:22:40 -0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,12 +1,7 @@
 #!/bin/sh
 
-echo "--- A iniciar o processo de deploy ---"
+echo "--- A iniciar o processo de deploy com Application Factory ---"
 
-# Passo 1: Executar o script de inicialização do banco de dados
-# Usamos 'python -m src.init_db' para executar o ficheiro como um módulo
-echo "A executar o script de inicialização do banco de dados..."
-python -m src.init_db
-
-# Passo 2: Iniciar o servidor Gunicorn
-echo "A iniciar o servidor Gunicorn..."
-gunicorn --bind 0.0.0.0:$PORT src.main:app
+# Inicia o Gunicorn chamando a função create_app()
+# O formato é 'modulo:nome_da_funcao()'
+gunicorn --bind 0.0.0.0:$PORT 'src.main:create_app()'

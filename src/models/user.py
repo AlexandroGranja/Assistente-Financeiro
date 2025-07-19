0ed3821d3f58b902b65105f586934a04cabf1323
Alexandro Granja <Alex.granjaaa@Hotmail.com>
2025-07-19 14:35:35 -0300

    Update user.py

--- src/models/user.py
+++ src/models/user.py
@@ -4,15 +4,13 @@ db = SQLAlchemy()
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), unique=True, nullable=False)
-    email = db.Column(db.String(120), unique=True, nullable=False)
+    phone_number = db.Column(db.String(50), unique=True, nullable=False) # Campo necessário
 
     def __repr__(self):
-        return f'<User {self.username}>'
+        return f'<User {self.phone_number}>'
 
     def to_dict(self):
         return {
             'id': self.id,
-            'username': self.username,
-            'email': self.email
+            'phone_number': self.phone_number,
         }
